bb4a05294b262d36074b8c2b53c4c0690afd91cb
Maya Eshed <maya.eshed@post.runi.ac.il>
2024-11-16 13:04:08 +0200

    1-4 questions testing

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+	    int numOfTerms = Integer.parseInt(args[0]);
+		double sum = 0.0;
+		for (int i = 0; i < numOfTerms; i++) {
+			// 1.0 / (2 * i + 1)= 1, 1/3, 1/5, 1/7 ...
+			double term = 1.0 / (2 * i + 1);
+			// the even index are (+) and the odd are (-)
+			if (i % 2 == 0) {
+				sum += term;
+			} else {
+				sum -= term;
+			}
+		}
+		double approxPI = sum * 4;
+		System.out.println("pi according to java: " + Math.PI);
+		System.out.println("pi, approximated:   " + approxPI);		
+	}	
 }
+
