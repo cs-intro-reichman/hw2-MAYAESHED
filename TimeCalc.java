7b908a0ccf4af7a7eb998c57a7a47695dcddd021
Maya Eshed <maya.eshed@post.runi.ac.il>
2024-11-16 03:05:44 +0200

    first, second and third questions testing

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,6 +1,5 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
         int minutesToAdd = Integer.parseInt(args[1]);
         int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
         int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
